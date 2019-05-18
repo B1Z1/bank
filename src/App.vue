427ca3f9ac812dd1724ff0d181726cb67e14c54e
ilyamishkin <iluhamishkin@gmail.com>
2019-05-19 01:55:50 +0200

    Added logo to sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<div>
-		<h1>asd</h1>
+		<sidebar></sidebar>
 	</div>
 </template>
 
--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,6 +1,17 @@
 <template>
-	<div class="l-sidebar">
-		asd
+	<div class="l-sidebar col-mbl-3 ui-p--resetrow">
+		<div class="c-wrapper ui-backcolor--sidebar ui-color--cwhite ui-p32px">
+
+			<div class="c-logo">
+				<p>Zarządzanie</p>
+				<h6 class="ui-mt8px">Bankami Światowymi</h6>
+			</div>
+
+			<nav class="c-navigation">
+
+			</nav>
+			
+		</div>
 	</div>
 </template>
 
@@ -12,5 +23,6 @@ export default {
 
 
 <style lang="scss">
+@import "../assets/scss/components/c-wrapper";
 @import "../assets/scss/layouts/l-sidebar";
 </style> 
